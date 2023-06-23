00ee665fec20e16e04fe635806b8f32fb60278a0
David Stevenson <drstevenson1958@yahoo.com>
2023-06-23 09:22:03 -0400

    Show error from trying to use ORDER BY when creating view

--- 003_use_order_by_in_outer_query.sql
+++ 003_use_order_by_in_outer_query.sql
@@ -3,17 +3,20 @@
 ---------------------------------------------------------------------
 
 -- ORDER BY in a View is not Allowed
---ALTER VIEW Sales.CustsWithOrders
---AS
+ALTER VIEW Sales.CustsWithOrders
+AS
 
---SELECT country, custid, companyname, contactname, contacttitle,
---  address, city, region, postalcode, phone, fax
---FROM Sales.Customers AS C
---WHERE EXISTS
---  (SELECT * FROM Sales.Orders AS O
---   WHERE O.custid = C.custid)
---ORDER BY country;
---GO
+SELECT country, custid, companyname, contactname, contacttitle,
+  address, city, region, postalcode, phone, fax
+FROM Sales.Customers AS C
+WHERE EXISTS
+  (SELECT * FROM Sales.Orders AS O
+   WHERE O.custid = C.custid)
+ORDER BY country;
+GO
+
+--Msg 1033, Level 15, State 1, Procedure CustsWithOrders, Line 10 [Batch Start Line 5]
+--The ORDER BY clause is invalid in views, inline functions, derived tables, subqueries, and common table expressions, unless TOP, OFFSET or FOR XML is also specified.
 
 -- Instead, use ORDER BY in Outer Query
 -- Note: Can't use ORDER BY in VIEW unless TOP or FOR XML is used.
