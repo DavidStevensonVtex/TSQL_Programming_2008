c47c8fdf4246c8eafff767d9cc5293af38e576db
David Stevenson <drstevenson1958@yahoo.com>
2023-06-24 13:11:50 -0400

    Mark as Special Procedure and Test

---
+++ ch03/006_mark_as_special_procedure.sql
@@ -0,0 +1,2 @@
+USE master ;
+EXEC sp_MS_marksystemobject 'dbo.sp_Proc1' ;
---
+++ ch03/007_test_special_procedure.sql
@@ -0,0 +1,7 @@
+USE InsideTSQL2008 ;
+EXEC dbo.sp_Proc1 ;
+
+USE AdventureWorks ;
+EXEC dbo.sp_Proc1 ;
+
+EXEC InsideTSQL2008.dbo.sp_Proc1 ;
