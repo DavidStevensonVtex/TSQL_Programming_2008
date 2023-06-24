d5066f713afe4473779fd17f4aeeac24d7743773
David Stevenson <drstevenson1958@yahoo.com>
2023-06-24 14:10:59 -0400

    The reasons to use Table Types, and the mandatory use of READ ONLY for table valued parameters

--- ch03/014_using_table_types_example.sql
+++ ch03/014_using_table_types_example.sql
@@ -16,3 +16,14 @@ GO
 --3           10249
 
 --(3 rows affected)
+
+-- Such use of table types as the type for table variables prevents you from the need 
+-- to repeat the table definition. Of course, it's not just about code brevity. The
+-- real news in supporting table types is that you can use those as types for input
+-- parameters in stored procedures and UDFs. Client APIs were also enhanced to support
+-- passing table-valued parameters to routines, so you are not restricted to using T-SQL
+-- to invoke such routines.
+
+-- Note that when defining a table-valued parameter in a routine, you have to specify the
+-- attribute READONLY, indicating that you can only read from the parameter but not write to
+-- it. For now, this attribute is mandatory.
