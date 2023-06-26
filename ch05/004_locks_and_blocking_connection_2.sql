f986af776a4194f32fccbb933897e1ab58f923f1
David Stevenson <drstevenson1958@yahoo.com>
2023-06-25 20:36:51 -0400

    Setting a LOCK_TIMEOUT before a query

--- ch05/004_locks_and_blocking_connection_2.sql
+++ ch05/004_locks_and_blocking_connection_2.sql
@@ -16,3 +16,9 @@ GO
 --SET LOCK_TIMEOUT 1800;  
 --SELECT @@LOCK_TIMEOUT AS [Lock Timeout];  
 --GO  
+
+-- Connection 2
+-- Stop, then set the LOCK_TIMEOUT, then retry
+-- SET LOCK_TIMEOUT 5000;
+-- SELECT keycol, col1, col2 FROM dbo.T1;
+-- GO
