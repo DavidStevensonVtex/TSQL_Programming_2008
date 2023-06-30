d3a5fb15c82f235ebfd15b39a168d98aea47b408
David Stevenson <drstevenson1958@yahoo.com>
2023-06-30 10:50:12 -0400

    Dynamic SQL: Query the table representing the current year

--- ch09/001_simple_exec_examples.sql
+++ ch09/001_simple_exec_examples.sql
@@ -15,3 +15,12 @@ EXEC('IF OBJECT_ID(''dbo.Orders' + @year + ''', ''U'') IS NOT NULL
 CREATE TABLE dbo.Orders' + @year + '(orderid INT PRIMARY KEY);');
 GO
 -- dbo.Orders2023 table created in tempdb database.
+
+-- Query the table representing the current year
+DECLARE @year AS CHAR(4) = CAST(YEAR(CURRENT_TIMESTAMP) AS CHAR(4));
+EXEC('SELECT orderid FROM dbo.Orders' + @year + ';');
+GO
+--orderid
+-------------
+
+--(0 rows affected)
