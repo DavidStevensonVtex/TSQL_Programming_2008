e6dfbb2829b897fccdf4d9a3d01669a3f9273419
David Stevenson <drstevenson1958@yahoo.com>
2023-06-30 10:48:31 -0400

    Chapter 9: Dynamic SQL
    Simple EXEC Example

---
+++ ch09/001_simple_exec_examples.sql
@@ -0,0 +1,17 @@
+---------------------------------------------------------------------
+-- EXEC
+---------------------------------------------------------------------
+
+---------------------------------------------------------------------
+-- Simple EXEC Example
+---------------------------------------------------------------------
+
+-- Sample data
+USE tempdb;
+
+DECLARE @year AS CHAR(4) = CAST(YEAR(CURRENT_TIMESTAMP) AS CHAR(4));
+EXEC('IF OBJECT_ID(''dbo.Orders' + @year + ''', ''U'') IS NOT NULL
+  DROP TABLE dbo.Orders' + @year + ';
+CREATE TABLE dbo.Orders' + @year + '(orderid INT PRIMARY KEY);');
+GO
+-- dbo.Orders2023 table created in tempdb database.
