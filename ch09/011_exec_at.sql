70ac2fb6b69863d7944be98a8d14f50d28a389e9
David Stevenson <drstevenson1958@yahoo.com>
2023-06-30 17:30:07 -0400

    EXEC AT - Local Working Example

--- ch09/011_exec_at.sql
+++ ch09/011_exec_at.sql
@@ -3,8 +3,8 @@
 ---------------------------------------------------------------------
 
 -- Create a linked server
-EXEC sp_addlinkedserver [Dojo], 'SQL Server';
-GO
+--EXEC sp_addlinkedserver [Dojo], 'SQL Server';
+--GO
 
 -- Using constant
 EXEC
@@ -12,8 +12,13 @@ EXEC
  'SELECT productid, productname, unitprice
 FROM InsideTSQL2008.Production.Products
 WHERE ProductID = ?;', 3
-) AT [Dojo];
+) AT [DSTEVENSONWIN10\SQL_2019];
 GO
+
+--productid   productname                              unitprice
+------------- ---------------------------------------- ---------------------
+--3           Product IMEHJ                            10.00
+
 
 -- Using variable
 DECLARE @pid AS INT;
@@ -23,8 +28,11 @@ EXEC
  'SELECT productid, productname, unitprice
 FROM InsideTSQL2008.Production.Products
 WHERE ProductID = ?;', @pid
-) AT [Dojo];
+) AT [DSTEVENSONWIN10\SQL_2019];
 GO
+--productid   productname                              unitprice
+------------- ---------------------------------------- ---------------------
+--3           Product IMEHJ                            10.00
 
 -- Using variables for both text and input
 DECLARE @sql AS NVARCHAR(500), @pid AS INT;
@@ -35,5 +43,9 @@ FROM InsideTSQL2008.Production.Products
 WHERE ProductID = ?;'
 SET @pid = 3;
 
-EXEC(@sql, @pid) AT [Dojo];
+EXEC(@sql, @pid) AT [DSTEVENSONWIN10\SQL_2019];
 GO
+
+--productid   productname                              unitprice
+------------- ---------------------------------------- ---------------------
+--3           Product IMEHJ                            10.00
